fb83b18b7e4f4aa0a311887c8368a8b075099934
varshamenon4 <menon.v@husky.neu.edu>
2020-09-16 15:15:26 -0400

    newsLoader.py

--- newsLoader.py
+++ newsLoader.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import requests
+from datetime import date
 
 # Get most popular article based on topic
 api_key = "7a6eb10b9fe649d89880147964ba83ca"
 topic = 'Black Lives Matter'
 # Add date
+today = str(date.today())
+print(str(today))
 
 url = ('http://newsapi.org/v2/everything?'
        'q=' + topic + '&'
-       'from=2020-09-03&'
+       'from=' + today + '&'
        'sortBy=popularity&'
        'language=en&'
        'apiKey=' + api_key)
@@ -21,4 +24,8 @@ print(r_json['articles'][0]['title'])
 print(r_json['articles'][0]['url'])
 
 # Get full contents of article
+rtcl_url = r_json['articles'][0]['url']
 
+rtcl_r = requests.get(rtcl_url)
+
+#print(rtcl_r.text)
